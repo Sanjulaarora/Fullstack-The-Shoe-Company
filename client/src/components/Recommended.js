b0ace75e62a2a6652df63959120c776004086119
Sanjulaarora <sanjulaarora3005@gmail.com>
2024-10-18 18:00:43 +0530

    made it better

--- client/src/components/Recommended.js
+++ client/src/components/Recommended.js
@@ -8,21 +8,37 @@ const Recommended = () => {
     <section id="recommended" className="px-6 md:px-12 max-w-[1440px] mx-auto">
       <h1 className="text-sm lg:text-2xl font-bold ml-[130px] media450:ml-[250px] media830:ml-[340px]">Recommended</h1>
       <div className="flex ml-[130px] media450:ml-[250px] media830:ml-[340px]">
-        <button className="btn" 
-        onClick={handleClick} 
-        value="Adidas">Adidas</button>
+        <button 
+          className="btn" 
+          onClick={handleClick} 
+          value="Adidas"
+        > 
+          Adidas 
+        </button>
 
-        <button className="btn" 
-        onClick={handleClick} 
-        value="Nike">Nike</button>
+        <button 
+          className="btn" 
+          onClick={handleClick} 
+          value="Nike"
+        >
+          Nike
+        </button>
 
-        <button className="btn" 
-        onClick={handleClick} 
-        value="Puma">Puma</button>
-
-        <button className="btn" 
-        onClick={handleClick} 
-        value="Vans">Vans</button>
+        <button 
+          className="btn" 
+          onClick={handleClick} 
+          value="Puma"
+        >
+          Puma
+        </button>
+        
+        <button 
+          className="btn" 
+          onClick={handleClick} 
+          value="Vans"
+        >
+          Vans
+        </button>
       </div>
     </section>
   )
